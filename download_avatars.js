c8664e923b069fa8f644bef73c9a7fd8e4d86f7e
S-Marks <marks.cherie.art@gmail.com>
2019-06-04 03:53:54 +0000

    Downloaded avatars

--- download_avatars.js
+++ download_avatars.js
@@ -7,8 +7,6 @@ function getRepoContributors(repoOwner, repoName, cb) {
     headers: {
       'User-Agent': 'request'
     }
-  // var result = "";
-  // cb(JSON.parse(result))
   };
 
   request(options, function(err, res, body) {
@@ -19,10 +17,23 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  // console.log("Result:", result);
   result.forEach((item) => {
-    console.log(item.avatar_url);
+    // console.log(item.avatar_url);
+    var filePath = "./pictures/" + item.login + ".jpg"
+    downloadImageByURL(item.avatar_url, filePath)
   })
 });
+
+function downloadImageByURL(url, filePath) {
+
+  request.get(url)               // Note 1
+         .on('error', function (err) {                                   // Note 2
+           throw err;
+         })
+         .on('response', function (response) {                           // Note 3
+           console.log(response.statusMessage, response.headers['content-type']);
+         })
+         .pipe(fs.createWriteStream(filePath));
+};
 
 console.log('Welcome to the GitHub Avatar Downloader!');
