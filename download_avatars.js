0881a64aa53be7e912032bdcc0a01c43b6ec44a9
S-Marks <marks.cherie.art@gmail.com>
2019-06-04 03:20:38 +0000

    Printed avatar_urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,28 @@
 var request = require('request');
+var fs = require('fs')
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  
-  getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log("Errors:", err);
-    console.log("Result:", result);
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  // var result = "";
+  // cb(JSON.parse(result))
+  };
+
+  request(options, function(err, res, body) {
+    let contributorObj = JSON.parse(res.body);
+    cb(err, contributorObj);
   });
 }
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  // console.log("Result:", result);
+  result.forEach((item) => {
+    console.log(item.avatar_url);
+  })
+});
 
 console.log('Welcome to the GitHub Avatar Downloader!');
